58b1b6ea0566ed3baeaba70458fd926f4cae7ec6
vishaldethe15 <dethevishal9@gmail.com>
2025-01-15 12:37:11 +0530

    Add navigation links

---
+++ app/components/Navbar/NavLinks.jsx
@@ -0,0 +1,37 @@
+const navLinkData = [
+  {
+    title: "Home",
+    url: "#",
+  },
+  {
+    title: "About",
+    url: "#",
+  },
+  {
+    title: "Features",
+    url: "#",
+  },
+];
+
+const NavLinks = () => {
+  return (
+    <nav className="hidden gap-12 lg:flex">
+      {navLinkData.map((link, index) => (
+        <NavLink title={link.title} url={link.url} key={index} />
+      ))}
+    </nav>
+  );
+};
+
+const NavLink = ({ title, url }) => {
+  return (
+    <a
+      href={url}
+      className="text-lg font-normal text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+    >
+      {title}
+    </a>
+  );
+};
+
+export default NavLinks;
