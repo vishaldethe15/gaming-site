94ecab896dcbf7767b5758223788a0f943509a9f
vishaldethe15 <dethevishal9@gmail.com>
2025-01-28 20:14:44 +0530

    Refactor Homepage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
-// pages/index.js
-import GamePage from './components/Pages/GamePage';
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/Hero";
 
-export default function Home() {
-  return <GamePage />;
-}
+const HomePage = () => {
+  return (
+    <div className="pb-4 sm:pb-8 lg:pb-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <Navbar />
+        <HeroSection />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
