c0df686ffa31136413640d2ba8c75a5536f3370c
vishaldethe15 <dethevishal9@gmail.com>
2025-01-28 20:31:57 +0530

    Remove navbar

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
-import Navbar from "./components/Navbar";
 import HeroSection from "./components/Hero";
 
 const HomePage = () => {
   return (
-    <div className="pb-4 sm:pb-8 lg:pb-12">
-      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <Navbar />
-        <HeroSection />
-      </div>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 };
 
